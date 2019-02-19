d6bb1186cbd1a1a92510ca14c41afb3865fe182f
dansgithub <danielniemann@protonmail.com>
2019-02-20 00:54:22 +0100

    add scss

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,14 +5,15 @@ class Board extends Component {
     state = {
         field: new Array(9).fill(null),
         player: true,
-        reset: null
+        reset: null,
+        winner: null
     }
 
     setField = (i) => {
         const { field, player } = this.state;
 
         if(field[i] === null) {
-            player ? field[i] = 0 : field[i] = 1;
+            player ? field[i] = "o" : field[i] = "x";
 
             this.setState({
                 field: field,
@@ -20,27 +21,46 @@ class Board extends Component {
             })
         }
 
-        // TODO: finish calculation of winner
+        // TODO: finish calculation of winner for both
         this.calculateWinner();
     }
 
     resetGame = () => {
         const freshField = new Array(this.state.field.length).fill(null);
-        this.setState({field: freshField})
+
+        this.setState({
+            field: freshField,
+            winner: null
+        })
     }
 
     calculateWinner = () => {
-        const {field} = this.state;
+        const { field } = this.state;
 
-        // horizoncal rows
+
         for(let i = 0; i <= 6; i+=3 ) {
-            if(field[i] === 0  && field[i + 1] === 0 && field[i + 2] === 0) {
-                console.log('wins');
+            if(field[i] === "o"  && field[i + 1] === "o" && field[i + 2] === "o") {
+                this.setState({
+                    winner: true
+                })
             }
         }
 
-        // vertical rows
-        // diagonal rows
+        for(let i = 0; i <=6; i++) {
+            if(field[i] === "o" && field[3 + i] === "o" && field[6 + i] === "o") {
+                this.setState({
+                    winner: true
+                })
+            }
+        }
+
+        for(let i = 0; i <=2; i+=2) {
+            if(field[i] === "o" && field[4] === "o" && field[8 - i] === "o") {
+                this.setState({
+                    winner: true
+                })
+            }
+        }
     }
 
     render() {
@@ -53,11 +73,14 @@ class Board extends Component {
                                 key={i}
                                 player={this.state.field[i]}
                                 onClick={() => this.setField(i)}
+                                i = {i}
                             />
                         )
                     })
                 }
                 <button onClick={this.resetGame}>reset</button>
+
+                winner: {this.state.winner ?  "there is a winner" : "not yet"}
             </div>
         )
     }
--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -4,16 +4,18 @@ const Field = (props) => {
     const styles = ['Field-item'];
 
     switch(props.player) {
-        case 0:
+        case "o":
             styles.push('Field-o');
             break;
-        case 1:
+        case "x":
             styles.push('Field-x');
             break;
+        default:
+            styles.push('Field-item');
     }
 
     return (
-        <div className={styles.join(' ')} onClick={props.onClick}></div>
+        <div className={styles.join(' ')} onClick={props.onClick}>{props.i}</div>
     )
 }
 
